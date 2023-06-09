851510946508bd97c28c4c0c9ef8b56d355f102e
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-09 20:52:59 +0530

    fetch all transaction

--- server/server.js
+++ server/server.js
@@ -19,6 +19,11 @@ app.get('/', (req, res) => { //get request
     res.send('Hello World');
 });
 
+app.get('/transaction', async (req, res) => { 
+    const transactions = await Transaction.find({}).sort({createdAt: -1});  //finds all the transactions. -1 is used to sort in latest to oldest  
+    res.json({data:transactions});
+});
+
 app.post('/transaction', async (req, res) => {  //post request
     const {amount, detail, date} = req.body;
     const transaction = new Transaction({
