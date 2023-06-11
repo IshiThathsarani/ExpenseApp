b3de1bad2822a4591813d4257077e827e5c29eec
IshiThathsarani <thathsaranisiriwardena@gmail.com>
2023-06-11 12:23:11 +0530

    login user

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,9 +3,8 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
+import {Link as RouterLink} from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -62,10 +61,6 @@ export default function Login() {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
@@ -75,15 +70,12 @@ export default function Login() {
               Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
+                <RouterLink to="/register">
+                    <Link component="span">
+                    {"Don't have an account? Sign Up"}
+                    </Link>
+                </RouterLink>                
               </Grid>
             </Grid>
           </Box>
